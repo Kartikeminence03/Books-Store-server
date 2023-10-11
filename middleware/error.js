9c5c23ad03d1357153d4a446b75cda878958228b
Kartikeminence03 <146444382+Kartikeminence03@users.noreply.github.com>
2023-10-11 17:55:59 +0530

    Update error.js

--- middleware/error.js
+++ middleware/error.js
@@ -7,25 +7,25 @@ module.exports = (err, req, res, next) => {
     // wrong mongodb id error
   if (err.name === "CastError") {
     const message = `Resources not found with this id.. Invalid ${err.path}`;
-    err = new ErrorHandler(message, 400);
+    err = ErrorHandler(message, 400);
   }
 
   // Duplicate key error
   if (err.code === 11000) {
     const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
-    err = new ErrorHandler(message, 400);
+    err = ErrorHandler(message, 400);
   }
 
   // wrong jwt error
   if(err.name = 'JsonWebTokenError'){
     const message = `Your url is invalid please try again letter`;
-    err = new ErrorHandler(message, 400)
+    err = ErrorHandler(message, 400)
   }
 
   // jwt expired
   if (err.name === "TokenExpiredError") {
     const message = `Your Url is expired please try again letter!`;
-    err = new ErrorHandler(message, 400);
+    err = ErrorHandler(message, 400);
   }
 
   res.status(err.statusCode).json({
